101a7e899afd8af875e23ff3c4d993b9ebe75792
Yuki Ogata <reallywannaknowthings@gmail.com>
2026-06-09 08:59:54 +0900

    update home cta layout

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,9 @@ export default function HomePage() {
     <>
       <FloatingContactCta />
       <Hero />
+      <AnnouncementsSection />
       <WorksPreviewSection />
       <HomeCtaSection />
-      <AnnouncementsSection />
       <FAQSection />
     </>
   );
